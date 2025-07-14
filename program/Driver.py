75c71064d55c4b5bf656a3cc43c8ddabf7a60aca
Alejandro Ortega <ort18248@uvg.edu.gt>
2025-07-13 21:30:35 -0600

    Add comments to Drive.py

--- program/Driver.py
+++ program/Driver.py
@@ -4,10 +4,10 @@ from MiniLangLexer import MiniLangLexer
 from MiniLangParser import MiniLangParser
 
 def main(argv):
-    input_stream = FileStream(argv[1])
-    lexer = MiniLangLexer(input_stream)
-    stream = CommonTokenStream(lexer)
-    parser = MiniLangParser(stream)
+    input_stream = FileStream(argv[1])      # Read src file as char stream
+    lexer = MiniLangLexer(input_stream)     # Create a lexer for the char stream
+    stream = CommonTokenStream(lexer)       # Lex the char stream into a token stream
+    parser = MiniLangParser(stream)         # Create a parser for the token stream
     tree = parser.prog()  # We are using 'prog' since this is the starting rule based on our MiniLang grammar, yay!
 
 if __name__ == '__main__':
